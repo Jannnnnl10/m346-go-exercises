841eb8b6535a47cf7882659ffef8fea7660b98d2
Jan Lehmann <Jan_Lehmann@sluz.ch>
2025-11-24 14:27:19 +0100

    Ex 2.2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -5,11 +5,17 @@ import "fmt"
 func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
-	fibs[2] = fibs[0] + fibs[1]
-	// TODO: correct up to index 4 using direct element access
+	 fibs[2] = fibs[0] + fibs[1] // 2
+ 	fibs[3] = fibs[1] + fibs[2] // 3
+	 fibs[4] = fibs[2] + fibs[3] // 5
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
-	// TODO: compute three more Fibonacci numbers and append them
+ // nächste zahl anhängen
+ fibs = append(fibs, fibs[3]+fibs[4]) // 8
+
+ // noch drei weitere anhängen
+ fibs = append(fibs, fibs[len(fibs)-1]+fibs[len(fibs)-2]) // 13
+ fibs = append(fibs, fibs[len(fibs)-1]+fibs[len(fibs)-2]) // 21
+ fibs = append(fibs, fibs[len(fibs)-1]+fibs[len(fibs)-2]) // 34
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
 }
