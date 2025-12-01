a3c6a3275298b5ebf5f98ea8c13ae77838d42054
Jan Lehmann <Jan_Lehmann@sluz.ch>
2025-12-01 14:15:47 +0100

    GO Block 4 Exercise 1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"fmt"
+)
+
 // TODO: implement the function computeGrade
+
+func computeGrade(gotPoints, maxPoints float64) float64{
+	return 1.0 + (gotPoints/maxPoints)*5.0
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0))
 }
