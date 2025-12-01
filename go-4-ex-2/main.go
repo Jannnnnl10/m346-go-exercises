9e0eb11c59e34bc7216fe643c2286536a612cfea
Jan Lehmann <Jan_Lehmann@sluz.ch>
2025-12-01 14:28:43 +0100

    GO Block 4 Exercise 2 - Status: DONE

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,17 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import ( "fmt" ; "math")
 
+// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypotenuse(a,b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
 func main() {
 	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(1,5))
+	fmt.Println(computeHypotenuse(2,4))
+	fmt.Println(computeHypotenuse(3,3))
+	fmt.Println(computeHypotenuse(4,2))
+	fmt.Println(computeHypotenuse(5,1))
+
 }
