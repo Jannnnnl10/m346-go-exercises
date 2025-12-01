751f6ee6b27597843bfa18301f425365d25d7cfe
Jan Lehmann <Jan_Lehmann@sluz.ch>
2025-12-01 14:31:52 +0100

    GO Block 4 Exercise 3 & 4 - Status: DONE

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeQuadraticFormula
+func computeQuadraticFormula(a, b, c float64) (float64, float64) {
+	discriminant := math.Pow(b, 2) - 4*a*c
+
+	if discriminant < 0 {
+		return math.NaN(), math.NaN()
+	}
+
+	x1 := (-b + math.Sqrt(discriminant)) / (2 * a)
+	x2 := (-b - math.Sqrt(discriminant)) / (2 * a)
+	return x1, x2
+}
 
 func main() {
 	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(1, -3, 2)) // 2, 1
+	fmt.Println(computeQuadraticFormula(1, 2, 1))  // -1, -1
+	fmt.Println(computeQuadraticFormula(1, 1, 1))  // NaN, NaN
 }
--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,23 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function convertCelsiusToFahrenheit
+func convertCelsiusToFahrenheit(c float64) float64 {
+	return c*9/5 + 32
+}
+
 // TODO: implement the function convertFahrenheitToCelsius
+func convertFahrenheitToCelsius(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
 
 func main() {
 	// TODO: call the function convertCelsiusToFahrenheit
+	fmt.Println(convertCelsiusToFahrenheit(0))   // 32
+	fmt.Println(convertCelsiusToFahrenheit(100)) // 212
+
 	// TODO: call the function convertFahrenheitToCelsius
+	fmt.Println(convertFahrenheitToCelsius(32))  // 0
+	fmt.Println(convertFahrenheitToCelsius(212)) // 100
 }
